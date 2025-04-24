e55e8a7088c27b964c6954c9850ed2df43abaf6d
piaqi001 <piaqi010@gmail.com>
2025-04-24 08:58:45 +0800

    fix: 修复 queryAll 未定义问题，部署新版前端

--- api/holders.js
+++ api/holders.js
@@ -1,4 +1,7 @@
 
 module.exports = (req, res) => {
-  res.status(200).json([{ wallet: "DEMO11111111111111111111111111111111111111", amount: 123.456 }]);
+  res.status(200).json([
+    { wallet: "7yZAbCdEfG1234567890abcdefABCDEF12345678", amount: 987654.321 },
+    { wallet: "6xYwVutsRq9876543210fedcbaFEDCBA987654321", amount: 123456.789 }
+  ]);
 };
